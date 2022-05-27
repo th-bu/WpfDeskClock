cb7ae93ebd04e3f9b797906d0acf49a0bde18b6d
Jeddix <2885473+Jeddix@users.noreply.github.com>
2022-05-27 20:42:27 +0200

    Initial commit

---
+++ WpfDeskClock/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿namespace WpfDeskClock;
+
+using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private readonly DispatcherTimer timer = new();
+    private readonly TimeSpan checkTime;
+    private DateTime displayDateTime = DateTime.Now;
+    private Brush fontColor = Brushes.White;
+
+    public MainViewModel()
+    {
+        this.FontFamily = new FontFamily(ConfigurationManager.AppSettings["Font"] ?? "Segoe UI");
+        this.checkTime = TimeSpan.Parse(ConfigurationManager.AppSettings["CheckTime"] ?? "09:16:00");
+
+        this.timer.Tick += this.TimerTick;
+        this.timer.Interval = new TimeSpan(0, 0, 1);
+        this.timer.Start();
+    }
+
+    public DateTime DisplayDateTime
+    {
+        get => this.displayDateTime;
+        private set
+        {
+            this.displayDateTime = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public Brush FontColor
+    {
+        get => this.fontColor;
+        private set
+        {
+            this.fontColor = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public FontFamily FontFamily { get; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void TimerTick(object? sender, EventArgs e)
+    {
+        DateTime now = DateTime.Now;
+        this.DisplayDateTime = now;
+
+        if (now.Hour >= this.checkTime.Hours && now.Minute >= this.checkTime.Minutes && now.Second >= this.checkTime.Seconds)
+        {
+            this.FontColor = Brushes.DeepSkyBlue;
+        }
+    }
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ WpfDeskClock/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace WpfDeskClock;
+
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Markup;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        // Set global culture to german
+        var culture = new CultureInfo("de-DE");
+        Thread.CurrentThread.CurrentUICulture = culture;
+        Thread.CurrentThread.CurrentCulture = culture;
+        LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+            new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+        this.InitializeComponent();
+    }
+}
